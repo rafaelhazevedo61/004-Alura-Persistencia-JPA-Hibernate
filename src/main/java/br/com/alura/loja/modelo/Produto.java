3f354a0a0b5b8cc40c0464f2f3f0d7a0479e26de
rafael <rafaelhazevedo61@gmail.com>
2022-05-24 23:44:03 -0300

    aula 2 - completa - persistindo uma entidade
    Signed-off-by: rafael <rafaelhazevedo61@gmail.com>

--- src/main/java/br/com/alura/loja/modelo/Produto.java
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -11,6 +11,7 @@ public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String descricao;
     private BigDecimal preco;
