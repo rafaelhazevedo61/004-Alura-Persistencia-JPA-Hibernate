54ba36b73a42aeffd3e236703a8a633161cd66db
rafael <rafaelhazevedo61@gmail.com>
2022-05-26 22:47:37 -0300

    aula 3 - parcial - organizando o código
    Signed-off-by: rafael <rafaelhazevedo61@gmail.com>

--- src/test/java/CadastroDeProduto.java
+++ src/test/java/CadastroDeProduto.java
@@ -1,4 +1,6 @@
-package br.com.alura.loja.modelo;
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -13,13 +15,11 @@ public class CadastroDeProduto {
         celular.setDescricao("Muito legal");
         celular.setPreco(new BigDecimal("800"));
 
-        //A String é o nome do persistence-unit
-        EntityManagerFactory factory = Persistence
-                .createEntityManagerFactory("loja");
-        EntityManager em = factory.createEntityManager();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao dao = new ProdutoDao(em);
 
         em.getTransaction().begin();
-        em.persist(celular);
+        dao.cadastrar(celular);
         em.getTransaction().commit();
         em.close();
     }
