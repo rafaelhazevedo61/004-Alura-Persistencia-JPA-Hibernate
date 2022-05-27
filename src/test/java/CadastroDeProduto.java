154f31bc622cd4bc523e00f009f226017b710d60
rafael <rafaelhazevedo61@gmail.com>
2022-05-26 22:55:01 -0300

    aula 3 - parcial - mais mapeamentos
    Signed-off-by: rafael <rafaelhazevedo61@gmail.com>

--- src/test/java/CadastroDeProduto.java
+++ src/test/java/CadastroDeProduto.java
@@ -1,4 +1,5 @@
 import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
@@ -10,10 +11,8 @@ import java.math.BigDecimal;
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
-        Produto celular = new Produto();
-        celular.setNome("Xiaomi Redmi");
-        celular.setDescricao("Muito legal");
-        celular.setPreco(new BigDecimal("800"));
+        Produto celular =
+                new Produto("Xiaomi Redmi","Muito legal",new BigDecimal("800"), Categoria.CELULARES);
 
         EntityManager em = JPAUtil.getEntityManager();
         ProdutoDao dao = new ProdutoDao(em);
