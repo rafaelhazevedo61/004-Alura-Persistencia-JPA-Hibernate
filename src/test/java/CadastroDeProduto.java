73dd511dda674cbd8a1de881514541cb58380d18
rafael <rafaelhazevedo61@gmail.com>
2022-05-26 23:05:40 -0300

    aula 3 - completa - mapeando relacionamentos
    Signed-off-by: rafael <rafaelhazevedo61@gmail.com>

--- src/test/java/CadastroDeProduto.java
+++ src/test/java/CadastroDeProduto.java
@@ -1,3 +1,4 @@
+import br.com.alura.loja.dao.CategoriaDao;
 import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
@@ -11,14 +12,20 @@ import java.math.BigDecimal;
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
+
+        Categoria celulares = new Categoria("CELULARES");
         Produto celular =
-                new Produto("Xiaomi Redmi","Muito legal",new BigDecimal("800"), Categoria.CELULARES);
+                new Produto("Xiaomi Redmi","Muito legal",new BigDecimal("800"), celulares);
 
         EntityManager em = JPAUtil.getEntityManager();
-        ProdutoDao dao = new ProdutoDao(em);
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
 
         em.getTransaction().begin();
-        dao.cadastrar(celular);
+
+        categoriaDao.cadastrar(celulares);
+        produtoDao.cadastrar(celular);
+
         em.getTransaction().commit();
         em.close();
     }
